96e9d405b574820343883821fa6304e1fbbef5cc
Nilo Mendonca <nilojrmendonca@gmail.com>
2020-12-08 17:55:11 -0300

    Realizacao dos exercicios de automatos

---
+++ arquivos/Automatos/dfa-prog/questao02_1.py
@@ -0,0 +1,21 @@
+def afd(delta, q, F, w):
+	for s in w:
+		try:
+			q = delta[(q, s)]
+		except:
+			return False
+	if q in F:
+		return True
+	return False
+
+delta = {('A', 'b'): 'B',
+		('B', 'a'): 'B',
+		('B', 'b'): 'D',
+		('B', 'c'): 'C',
+		('C', 'a'): 'B',
+		('D', 'b'): 'A'}
+
+if afd(delta, 'A', ['D'], input()):
+	print("Aceito")
+else:
+	print("Rejeitado")

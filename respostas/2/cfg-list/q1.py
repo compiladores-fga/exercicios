7bbc5919ec2970e92180e0733a7cd55ea91a65fe
Gabriel Carvalho <gabriel315df@gmail.com>
2020-12-12 13:42:08 -0300

    Added cfg-list

---
+++ respostas/2/cfg-list/q1.py
@@ -0,0 +1,24 @@
+from lark import Lark
+
+grammar = Lark(r"""
+?start  :   list
+?list   :   "[" ELEM ("," ELEM)* "]" -> json
+        |   "[" (ELEM? ","+)+ "]" -> js
+        |   "[" (ELEM ",")+ "]" -> python
+        |   "(" ELEM+ ")" -> lisp
+        |   "(" (ELEM ","?)+ ")" -> lispii
+ELEM    :   /[^\,\s\)\]]+/
+%ignore /\s+/
+""")
+
+exemplos = [
+  "[1, 2, 3]",
+  "(1 2 3)",
+  "(1, 2, 3,)",
+  "[1, 2, 3,]",
+  "[1,2,,3,,,]"
+]
+
+for ex in exemplos:
+    tree = grammar.parse(ex)
+    print(tree)

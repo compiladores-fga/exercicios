4bd283069d8bbd756031e8b818c74fca098dbb96
giovanadionisio <giovanadionisio.gd@gmail.com>
2020-12-12 22:15:50 -0300

    dfa_prog

---
+++ respostas/topico_3/dfa_prog/Q2.py
@@ -0,0 +1,36 @@
+#dfa_prog
+
+Q = {"A", "B", "C", "D"}
+
+Σ = {"a", "b", "c"}
+
+q0 = "B"
+
+F = {"C"}
+
+δ = {
+    ("A", "b"): "B",
+    ("B", "a"): "B",
+    ("B", "b"): "D",
+    ("B", "c"): "C",
+    ("C", "a"): "B",
+    ("D", "b"): "A",
+}
+
+
+def automato(δ, q0, F, st):
+    estado = q0
+
+    for c in st:
+        try:
+            estado = δ[estado, c]
+        except KeyError:
+            estado = None
+
+    return estado in F
+
+st = input("Entrada: ")
+if(automato(δ, q0, F, st)):
+    print("Aceito")
+else:
+    print("Rejeitado")

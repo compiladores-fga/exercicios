555695a4e3a5ce1dda3b00c3c4e749f82e036055
Danillo Gonçalves Souza <danillosouza1704@gmail.com>
2020-12-13 16:29:27 -0300

    Create aluno.py

---
+++ aluno.py
@@ -0,0 +1,5 @@
+# Identifique-se preenchendo as strings neste arquivo
+nome = "Danillo Goncalves de Souza"
+matricula = "17/0139981"
+email = "danillosouza1704@gmail.com"
+usuario_github = "DanilloGS"

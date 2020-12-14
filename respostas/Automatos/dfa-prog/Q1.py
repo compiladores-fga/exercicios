0a565d1d7671bcfeac78cc0212af9d7f292b2704
ilus1 <71859307+ilus1@users.noreply.github.com>
2020-12-14 14:09:32 -0300

    Add files via upload

---
+++ respostas/Automatos/dfa-prog/Q1.py
@@ -0,0 +1,42 @@
+# ===============================
+# Autômato Determinístico Finito
+# ===============================
+
+# Conjunto de estados válidos
+Q = {"A", "B", "C", "D"}
+
+# Alfabeto de entrada
+alfabeto = {"a", "b", "c"}
+
+# Estado inicial
+q0 = "A"
+
+# Conjunto de estados finais
+finais = {"D"}
+
+# Tabela de transição
+transicoes = {
+    ("A", "b"): "B",
+    ("B", "a"): "B",
+    ("B", "c"): "C",
+    ("B", "b"): "D",
+    ("D", "b"): "A",
+    ("C", "a"): "B",
+}
+
+
+def automaton(transicoes, q0, finais, input_string):
+    state = q0
+
+    for c in input_string:
+        old = state
+        try:
+            state = transicoes[state, c]
+        except KeyError:
+            state = None
+
+    return state in finais
+
+
+st = input("Entrada: ")
+print("Aceito")if automaton(transicoes, q0, finais, st) else print("Rejeitado")

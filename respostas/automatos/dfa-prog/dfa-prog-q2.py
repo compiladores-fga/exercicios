9fe8f1fc1f6b701fbb572e458cb875890961ba34
Pedro Rodrigues <peddroprp@gmail.com>
2020-12-14 18:31:43 -0300

    add more automated exercises

---
+++ respostas/automatos/dfa-prog/dfa-prog-q2.py
@@ -0,0 +1,22 @@
+def dfa_prog(delta, initial, accepting, text):
+    boolean = False
+    state = initial
+    try:
+        for c in text:
+            state = delta[state][c]
+        boolean = state in accepting 
+    except:
+        boolean = False
+    if boolean:
+        print("Aceito")
+    else: 
+        print("Rejeitado")
+
+dfa_map = {
+    'A':{'b': 'B'},
+    'B':{'a': 'B', 'b': 'D','c': 'C'},
+    'C':{'a': 'B'},
+    'D':{'b': 'A'}
+}
+
+dfa_prog(dfa_map, 'B', {'C'}, input().lower())

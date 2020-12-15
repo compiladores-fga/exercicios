ecec2388f8eb2affe0b346ae9c3e6859777613c3
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2020-12-15 12:20:09 -0300

    Adicionar autômatos dfa-prog (Q1 e Q2)
    Adicionar códigos em Python 3 como respostas das questões Q1 e Q2 da
    seção autômatos, competência `dfa-prog`, questões Q1 e Q2

---
+++ respostas/automatos/dfa-prog/q2.py
@@ -0,0 +1,44 @@
+state = None
+
+def state_A(data):
+    global state
+    if (data == 'b'):
+        state = state_B
+
+def state_B(data):
+    global state
+    if (data == 'a'):
+        state = state_B
+    elif (data == 'b'):
+        state = state_D
+    elif (data == 'c'):
+        state = state_C
+
+def state_C(data):
+    global state
+    if (data == 'a'):
+        state = state_B
+
+def state_D(data):
+    global state
+    if (data == 'b'):
+        state = state_A
+
+def state_machine(data):
+    global state
+    state = state_B
+    for c in data:
+        print(f"State: {state}, Input: {c}")
+        state(c)
+        print(f"Next state: {state}")
+        print()
+    if state == state_C:
+        print("Aceito")
+        return True
+    else:
+        print("Rejeitado")
+        return False
+
+if __name__ == "__main__":
+    data = input("Input string: ")
+    state_machine(data)

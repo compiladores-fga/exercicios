af43c5f4700914f0bab85be829a3048a0f1665df
= <jg.antunes.medeiros@gmail.com>
2020-12-16 13:25:27 -0200

    adicionando cfg-sexpr

---
+++ respostas/gramatica/cfg-sexpr/cfg-sexpr.py
@@ -0,0 +1,31 @@
+import re
+import math
+from lark import Lark, InlineTransformer, Token
+
+grammar = Lark(
+r"""
+    ?start: expr 
+    ?expr : expr "+" term -> add
+        | term
+    ?term : term "*" atom -> mul
+        | atom
+    ?atom : NUMBER -> number
+        | "(" expr ")"
+    NUMBER : /\d+/
+    %ignore /\s+/
+    %ignore /\#.*/
+"""
+)
+
+class Transformer(InlineTransformer):
+    def add(self, a, b):
+        return ["+", a, b]
+
+    def mul(self, a, b):
+        return ["*", a, b]
+    
+    def number(self, token):
+        try:
+            return int(token)
+        except:
+            return float(token)

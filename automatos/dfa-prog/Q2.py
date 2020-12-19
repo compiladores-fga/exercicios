6c294555db6df6020997d2ca570fae6a91ee99b1
aquiles <jjoseaquiless@gmail.com>
2020-12-18 23:40:26 -0300

    dfa-prog

---
+++ automatos/dfa-prog/Q2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+if __name__ == "__main__":
+	ent = input("digite a string que vai passar pelo automato\n")
+
+	automato = {
+		('A', 'b'): 'B',
+		('B', 'c'): 'C',
+		('B', 'b'): 'D',
+		('B', 'a'): 'B',
+		('C', 'a'): 'B',
+		('D', 'b'): 'A'
+	}
+
+	init = 'B'
+	fin = 'C'
+	try:
+		for x in ent:
+			init = automato[(init, x)]
+		if init == fin:
+			print("Aceito")
+		else:
+			print("Rejeitado")
+	except:
+		print("Rejeitado")
